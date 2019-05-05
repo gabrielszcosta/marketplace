21c8557c557cd8dd4513273e31180852563a9bf8
Gabriel Costa <gabrielszcosta@gmail.com>
2019-05-05 14:20:06 -0400

    Adição criação de usuarios, login e autenticação jwt

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,5 @@ const SessionController = require('./app/controllers/SessionController')
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
-routes.get('/teste', authMiddleware, (req, res) => {
-  res.json({ ok: true })
-})
 
 module.exports = routes
